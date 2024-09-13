02a7901f06dc5f4ec4f73337f4fbacdbe061a82b
Giuseppe Partipilo <gpartipilo726@gmail.com>
2024-09-13 17:05:45 -0400

    added styles for buttons and header. Added button functionality to pop up menu for adding a new book to the library display.

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const addBook = document.querySelector("#addbook-btn");
+const close = document.querySelector("#closebtn");
+
+addBook.addEventListener("click", () =>{
+const addBookForm = document.querySelector("#book_form");
+
+addBookForm.style.display = "flex";
+
+})
+
+addBook.addEventListener("click", () =>{
+    addBookForm.style.display = "none"; 
+    
+    })
